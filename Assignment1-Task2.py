f67d90e98d71a82af2d7b7381ff7b98c654c8a8d
Amarjoisms <77470761+Amarjoisms@users.noreply.github.com>
2025-03-28 18:46:22 +0530

    Add files via upload

---
+++ Assignment1-Task2.py
@@ -0,0 +1,7 @@
+a = input('Enter your first name: ')
+b = input('Enter your second name: ')
+
+fullname = (a +' '+ b + '!')
+print(fullname)
+
+print("Hello, "+fullname +' Welcome to the python program.')
